c2ebea61cf9433274dc03db32c15047f947cac52
TanLeJS <tanlengoc2000@gmail.com>
2024-04-04 22:21:24 -0400

    video 103

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,47 @@
 import MainSlider from "@/components/main/main.slider";
+import { sendRequest } from "@/utils/api";
 import { Container } from "@mui/material";
 export default async function HomePage() {
 
-  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+  // const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     category: "CHILL",
+  //     limit: 10
+  //   })
+  // })
+  // console.log(">>> check res sever:", await res.json())
+
+
+  const chills = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      category: "CHILL",
-      limit: 10
-    })
+    body: { category: "CHILL", limit: 10 }
   })
-  console.log(">>> check res sever:", await res.json())
+  const workouts = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: "http://localhost:8000/api/v1/tracks/top",
+    method: "POST",
+    body: { category: "WORKOUT", limit: 10 }
+  })
+  const party = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: "http://localhost:8000/api/v1/tracks/top",
+    method: "POST",
+    body: { category: "PARTY", limit: 10 }
+  })
   return (
     <Container>
-      <MainSlider />
-      <MainSlider />
-      <MainSlider />
+      <MainSlider
+        title="Top Chill"
+        data={chills?.data ?? []} />
+      <MainSlider
+        title="Top Workout"
+        data={workouts?.data ?? []} />
+      <MainSlider
+        title="Top Party"
+        data={party?.data ?? []} />
     </Container>
   );
 }
--- src/components/main/main.slider.tsx
+++ src/components/main/main.slider.tsx
@@ -8,15 +8,24 @@ import Slider, { Settings } from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-const MainSlider = () => {
+
+interface IProps {
+    data: ITrackTop[],
+    title: string
+}
+
+const MainSlider = (props: IProps) => {
+    const { data, title } = props
     const NextArrow = (props: any) => {
         return (
-            <Button variant="outlined"
+            <Button
+                color='inherit'
+                variant="contained"
                 onClick={props.onClick}
                 sx={{
                     position: "absolute",
-                    right: 0,
-                    top: "50%",
+                    right: 25,
+                    top: "25%",
                     zIndex: 2,
                     minWidth: 30,
                     width: 35,
@@ -29,7 +38,10 @@ const MainSlider = () => {
 
     const PrevArrow = (props: any) => {
         return (
-            <Button variant="outlined" onClick={props.onClick}
+            <Button
+                color='inherit'
+                variant="contained"
+                onClick={props.onClick}
                 sx={{
                     position: "absolute",
                     top: "50%",
@@ -57,47 +69,32 @@ const MainSlider = () => {
         <Box
             sx={{
                 margin: "0 50px",
-                ".abc": {
-                    padding: "0 10px"
+                ".track": {
+                    padding: "0 10px",
+                    "img": {
+                        height: 150,
+                        width: 150
+                    }
                 },
                 "h3": {
                     border: "1px solid #ccc",
                     padding: "20px",
                     height: "200px",
-
                 }
             }}
         >
-            <h2> Multiple tracks </h2>
+            <h2> {title} </h2>
 
             <Slider {...settings}>
-                <div className="abc">
-                    <h3>Track 1</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 2</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 3</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 4</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 5</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 6</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 7</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 8</h3>
-                </div>
-                <div className="abc">
-                    <h3>Track 9</h3>
-                </div>
+                {data.map(track => {
+                    return (
+                        <div className='track' key={track._id}>
+                            <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/${track.imgUrl}`} />
+                            <h4>{track.title}</h4>
+                            <h5>{track.description}</h5>
+                        </div>
+                    )
+                })}
             </Slider>
             <Divider />
         </Box>
