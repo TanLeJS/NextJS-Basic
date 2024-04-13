266af241e9e1b8e0979e169a6c79069ee0996f15
TanLeJS <tanlengoc2000@gmail.com>
2024-04-13 17:44:33 -0400

    video 154

--- src/app/(user)/profile/[slug]/page.tsx
+++ src/app/(user)/profile/[slug]/page.tsx
@@ -1,19 +1,29 @@
+import ProfileTrack from "@/components/header/profile.tracks"
 import { sendRequest } from "@/utils/api"
+import { Container, Grid } from "@mui/material"
 
 
-const ProfilePage = async ({ params }: { params: { slug: string } }) => {
-
-    const tracks = await sendRequest<IBackendRes<ITrackTop[]>>({
+const ProfileUserPage = async ({ params }: { params: { slug: string } }) => {
+    const slug = params.slug
+    const data = await sendRequest<IBackendRes<IModelPaginate<ITrackTop>>>({
         url: "http://localhost:8000/api/v1/tracks/users?current=1&pageSize=10",
         method: "POST",
-        body: { id: params.slug }
+        body: { id: slug }
     })
-    console.log(">>> check track", tracks)
+    const d = data?.data?.result ?? []
     return (
-        <div>
-            Hello with Profile page {params.slug}
-        </div>
+        <Container sx={{ my: 5 }}>
+            <Grid container spacing={5} >
+                {d.map((item: any, index: number) => {
+                    return (
+                        <Grid item xs={12} md={6} key={index}>
+                            <ProfileTrack data={item} />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Container>
     )
 }
 
-export default ProfilePage
+export default ProfileUserPage 
