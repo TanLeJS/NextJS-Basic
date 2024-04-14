60886dfa239e5fa5b7a77e346d07ca4e1689a03c
TanLeJS <tanlengoc2000@gmail.com>
2024-04-13 20:23:32 -0400

    video 159

--- src/components/header/profile.tracks.tsx
+++ src/components/header/profile.tracks.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useTrackContext } from '@/lib/track.wrapper';
 import { useTheme } from '@emotion/react';
+import PauseIcon from '@mui/icons-material/Pause';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
@@ -31,12 +32,34 @@ const ProfileTrack = (props: any) => {
                     <IconButton aria-label="previous">
                         {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
                     </IconButton>
-                    <IconButton aria-label="play/pause"
-                        onClick={() => setCurrentTrack({ ...data, isPlaying: false })}
 
-                    >
-                        <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                    </IconButton>
+                    {
+                        (data._id !== currentTrack._id ||
+                            data._id === currentTrack._id && currentTrack.isPlaying === false
+                        )
+                        &&
+                        <IconButton aria-label="play/pause"
+                            onClick={(e) => setCurrentTrack({ ...data, isPlaying: true })}
+                        >
+                            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                        </IconButton>
+                    }
+
+
+                    {
+                        (data._id === currentTrack._id && currentTrack.isPlaying === true)
+                        &&
+                        <IconButton aria-label="play/pause"
+                            onClick={(e) => setCurrentTrack({ ...data, isPlaying: false })}
+
+                        >
+                            <PauseIcon sx={{ height: 38, width: 38 }} />
+
+                        </IconButton>
+
+                    }
+
+
                     <IconButton aria-label="next">
                         {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
                     </IconButton>
