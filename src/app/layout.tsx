2b698881df286ebc5e726e264163f90cca67cc3e
TanLeJS <tanlengoc2000@gmail.com>
2024-05-01 14:44:43 -0400

    video 177

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import ThemeRegistry from '@/components/theme-registry/theme.registry';
 import NextAuthWrapper from '@/lib/next.auth.wrapper';
+import NProgressWrapper from '@/lib/nprogress.wrapper';
 import { TrackContextProvider } from '@/lib/track.wrapper';
 import { ToastProvider } from '@/utils/toast';
 
@@ -9,15 +10,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <NextAuthWrapper>
-          <ThemeRegistry>
-            <ToastProvider>
-              <TrackContextProvider>
-                {children}
-              </TrackContextProvider>
-            </ToastProvider>
-          </ThemeRegistry>
-        </NextAuthWrapper>
+        <ThemeRegistry>
+          <NextAuthWrapper>
+            <NProgressWrapper>
+              <ToastProvider>
+                <TrackContextProvider>
+                  {children}
+                </TrackContextProvider>
+              </ToastProvider>
+            </NProgressWrapper>
+          </NextAuthWrapper>
+        </ThemeRegistry>
+
+
       </body>
     </html>
   );
