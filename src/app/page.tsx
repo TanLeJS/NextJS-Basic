52aa927141dedd3eca58e0d31f71c7a9046c7cb6
TanLeJS <tanlengoc2000@gmail.com>
2024-05-05 21:28:30 -0400

    video 202

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 
 export default function Home() {
+  const handleLogin = async (formData: FormData) => {
+    "use server"
+    console.log(">>> check formData: username", formData.get("username"))
+    console.log(">>> check formData: password", formData.get("password"))
+  }
+
   return (
-    <div>
-      Hello world
+    <div style={{ marginLeft: "200px" }}>
+      <h2>HTML Forms</h2>
+      <form action={handleLogin}>
+        <label >Username</label><br />
+        <input type="text" name="username" />
+        <br />
+        <br />
+        <label >Password</label><br />
+        <input type="text" name="password" />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   )
 }
